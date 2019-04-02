0d15389e6721366994638573a6af446bb752b90f
Valentina Adami <valeadami@gmail.com>
2019-04-02 16:49:14 +0200

    listApp

--- index.js
+++ index.js
@@ -254,55 +254,7 @@ app.get('/testSessione', function(req, res, next) {
     }
    
   } 
-/*
- function autenticate(){
 
-    // Load client secrets from a local file.
-    fs.readFile('credentials.json', (err, content) => {
-      if (err) return console.log('Error loading client secret file:', err);
-      // Authorize a client with credentials, then call the Google Calendar API.
-      authorize(JSON.parse(content), WebhookProcessing);
-    });
- }
- function authorize(credentials, callback) {
-    const {client_secret, client_id, redirect_uris} = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
-  
-    // Check if we have previously stored a token.
-    fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) return getAccessToken(oAuth2Client, callback);
-      oAuth2Client.setCredentials(JSON.parse(token));
-      callback(oAuth2Client);
-    });
-  }
-  
-  
-  function getAccessToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-      rl.close();
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err) return console.error('Error retrieving access token', err);
-        oAuth2Client.setCredentials(token);
-        // Store the token to disk for later program executions
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-          if (err) return console.error(err);
-          console.log('Token stored to', TOKEN_PATH);
-        });
-        callback(oAuth2Client);
-      });
-    });
-  }
-  */
  function callAVA(agent) {
   return new Promise((resolve, reject) => {
  
@@ -378,8 +330,9 @@ function callAVANEW(agent) {
       switch (strRicerca) {
         case 'getAppuntamenti':
             console.log('sono nel getAppuntamenti con data richiesta '+ dataRichiesta);
-            agent.add('sono nel getAppuntamenti con data richiesta '+ dataRichiesta);
-            resolve(agent);
+           /* agent.add('sono nel getAppuntamenti con data richiesta '+ dataRichiesta);
+            resolve(agent);*/
+            listAppointment(dataRichiesta);
             break;
           
           
