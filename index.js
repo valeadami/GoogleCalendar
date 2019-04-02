0105b543eec17c41670085deb9e6114169039168
Valentina Adami <valeadami@gmail.com>
2019-04-02 21:40:13 +0200

    sto fixed key

--- index.js
+++ index.js
@@ -45,12 +45,13 @@ app.use(function (req, res, next) {
    
     req.session.client_email=process.env.GOOGLE_CLIENT_EMAIL;
     const fixedKey = process.env.GOOGLE_CLIENT_PRIVATE_KEY;
-  /*  fixedKey=fixedKey.replace(new RegExp("\\\\n", "\g"), "\n");
-    console.log('DOPO REPLACE DE sto cazzo de fixedKey '+ fixedKey);*/
-
     req.session.private_key= process.env.GOOGLE_CLIENT_PRIVATE_KEY; //fixedKey;
     console.log('sti cazzi de var de sessione in app.use '+ req.session.client_email + ', chiave '+req.session.private_key);
 
+    fixedKey=fixedKey.replace(new RegExp("\\\\n", "\g"), "\n");
+    console.log('DOPO REPLACE DE sto cazzo de fixedKey '+ fixedKey);
+
+  
     
    
     next();
