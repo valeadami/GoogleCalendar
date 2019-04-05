efea22498f935b9ebc22980af7c657208a0edf0b
Valentina Adami <valeadami@gmail.com>
2019-04-05 22:23:05 +0200

    orario4

--- index.js
+++ index.js
@@ -387,10 +387,10 @@ function callAVANEW(agent) {
 
                        /* start=start.toLocaleString('it-IT', { weekday: 'long',day: 'numeric', month: 'long',  timeZone: timeZone });
                         console.log('-----  DOPO start '+ start);*/
-                       var tmp=addHours(new Date(events[i].start.dateTime),2);
+                      // var tmp=addHours(new Date(events[i].start.dateTime),2); getLocaleTimeString
                        console.log('------------------- tmp = '+tmp);
-                       strTemp+= events[i].summary +' alle ore ' + (new Date(events[i].start.dateTime)).toLocaleTimeString() + '-' +tmp +'\n';
-                       
+                      // strTemp+= events[i].summary +' alle ore ' + (new Date(events[i].start.dateTime)).toLocaleTimeString()  +'\n';
+                      strTemp+= events[i].summary +' alle ore ' + getLocaleTimeString(events[i].start.dateTime)  +'\n';
                        console.log('strTemp ' + strTemp);
                      }
                 }   // fine if
